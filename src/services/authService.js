fd600b94979d73f9c1c50f340e3bc3bb6d542576
Jesus <jesus.cont5648@gmail.com>
2026-01-20 21:27:50 -0500

    vite api base url

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
 
-const API_URL = import.meta.env.API_BASE_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
 
 //concat /auth/login/
 const LOGIN_URL = `${API_URL}/auth/login/`;
